9134874526796976d58ffe9d9d0c5467e958823f
Nikolas Koesling <nikolas@koesling.info>
2022-03-18 10:19:31 +0100

    add license to help message

--- src/main.cpp
+++ src/main.cpp
@@ -7,9 +7,34 @@
 #include <unistd.h>
 
 int main(int argc, char **argv) {
-    if (argc != 2) {
-        std::cerr << "usage: " << std::endl;
-        std::cerr << "    dump_shm <shm_name>" << std::endl;
+    if (argc != 2 || std::string(argv[1]) == "-h" || std::string(argv[1]) == "--help") {
+        auto &out = argc != 2 ? std::cerr : std::cout;
+        out << "Dump the content of a shared memory to stdout" << std::endl;
+        out << "usage: " << std::endl;
+        out << "    dump_shm <shm_name>" << std::endl;
+        out << std::endl;
+        out << std::endl;
+        out << "MIT License:" << std::endl;
+        out << std::endl;
+        out << "Copyright (c) 2021 Nikolas Koesling" << std::endl;
+        out << std::endl;
+        out << "Permission is hereby granted, free of charge, to any person obtaining a copy" << std::endl;
+        out << "of this software and associated documentation files (the \"Software\"), to deal" << std::endl;
+        out << "in the Software without restriction, including without limitation the rights" << std::endl;
+        out << "to use, copy, modify, merge, publish, distribute, sublicense, and/or sell" << std::endl;
+        out << "copies of the Software, and to permit persons to whom the Software is" << std::endl;
+        out << "furnished to do so, subject to the following conditions:" << std::endl;
+        out << std::endl;
+        out << "The above copyright notice and this permission notice shall be included in all" << std::endl;
+        out << "copies or substantial portions of the Software." << std::endl;
+        out << std::endl;
+        out << "THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR" << std::endl;
+        out << "IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY," << std::endl;
+        out << "FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE" << std::endl;
+        out << "AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER" << std::endl;
+        out << "LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM," << std::endl;
+        out << "OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE" << std::endl;
+        out << "SOFTWARE." << std::endl;
         exit(EX_USAGE);
     }
 
